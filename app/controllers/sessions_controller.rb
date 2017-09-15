f19c498f612c4c82093a00c40ab79b609239df83
Vlad <Vlad@hotmail.co.nz>
2017-09-15 22:11:28 +1200

    finished login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  
+  def new
+
+  end 
+
+  def create
+  	@user = User.find_by(email: params[:session][:email].downcase)
+  	if @user && @user.authenticate(params[:session][:password])
+  	  log_in(@user) 
+  	  params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+  	  redirect_to posts_url
+  	else 
+  	flash.now[:danger] = "oooooh Shnap linden! incorrect email/password combination"
+    render 'new'
+  	end   	
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create' 
-  resources :sessions
-  resources :posts
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :delete]
+  resources :posts, only: [:index, :create]
   root 'static_pages#home'
 end
